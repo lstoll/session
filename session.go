b0e5f04cd9d1a9c71f8072e7ed98596fe656e1ab
Lincoln Stoll <me@lds.li>
2024-10-01 09:48:35 +0200

    more wip on interface

--- session.go
+++ session.go
@@ -2,17 +2,78 @@ package session
 
 import (
 	"context"
-	"time"
+	"net/http"
 )
 
-type Sessionable[T any] interface {
-	Type() string
-	NotAfter() time.Time
+type Sessionable interface {
+	New() Sessionable
 
-	*T
+	Expired() bool
 }
 
-func Load[T any, PT Sessionable[T]](ctx context.Context) (_ PT, exist bool, _ error) {
+type Store interface {
+	LoadSession(name string, r *http.Request, into any) error
+	SaveSession(name string, w http.ResponseWriter, r *http.Request, data []byte) error
+}
+
+type Manager[T Sessionable] struct {
+	init func() T
+}
+
+func NewManager[T any, PT interface {
+	Sessionable
+	*T
+}](name string, manager *Manager) *Manager[PT] {
+	return &Manager[PT]{
+		init: func() PT {
+			return PT(new(T))
+		},
+	}
+}
+
+func (m *Manager[T]) Wrap(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		sd, loaded, delete, err := m.loadSession(r)
+		if err != nil && m.opts.FailOnSessionLoadError {
+			m.opts.ErrorHandler(err, w, r)
+			return
+		}
+		if !loaded {
+			// either failed or loaded no cookie, init a new session.
+			sd = PtrT(new(T))
+		}
+
+		sess := &session[T, PtrT]{
+			data:   sd,
+			delete: delete,
+		}
+
+		r = r.WithContext(context.WithValue(r.Context(), mgrCtxKey{}, m))
+		r = r.WithContext(context.WithValue(r.Context(), mgrCtxKey{}, m))
+
+		r = r.WithContext(context.WithValue(r.Context(), sessCtxKey{sessName: m.sessionName}, sess))
+		hw := &hookRW{
+			ResponseWriter: w,
+			hook: func(w http.ResponseWriter) bool {
+				if err := m.writeSession(w, sess); err != nil {
+					m.opts.ErrorHandler(err, w, r)
+					return false
+				}
+				return true
+			},
+		}
+
+		next.ServeHTTP(hw, r)
+
+		// if the handler doesn't write anything, make sure we fire the hook
+		// anyway.
+		hw.hookOnce.Do(func() {
+			hw.hook(hw.ResponseWriter)
+		})
+	})
+}
+
+func (s *Manager[T]) Get(ctx context.Context) (_ T, exist bool) {
 	sessCtx, ok := ctx.Value(sessCtxKey{}).(*sessCtx)
 	if !ok {
 		panic("context contained no or invalid session")
